13052a7aa3b2972c67d6428d085619e791e819a5
sneha <snehavanka2006@gmail.com>
2026-07-09 20:53:30 +0530

    Add validation for AssignSkillsRequestDTO

--- src/main/java/com/organizational/knowledge_gap_platform/dto/AssignSkillsRequestDTO.java
+++ src/main/java/com/organizational/knowledge_gap_platform/dto/AssignSkillsRequestDTO.java
@@ -1,14 +1,13 @@
 package com.organizational.knowledge_gap_platform.dto;
-
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.List;
 
 @Getter
 @Setter
 public class AssignSkillsRequestDTO {
 
+    @NotEmpty(message = "Skill IDs cannot be empty")
     private List<Long> skillIds;
-
 }
