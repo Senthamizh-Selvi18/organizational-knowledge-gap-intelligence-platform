bd2c634ad13b676e4100a93bbed5fe808969feb2
sneha <snehavanka2006@gmail.com>
2026-07-12 00:38:20 +0530

    Implement Employee Management backend CRUD APIs

--- src/main/java/com/organizational/knowledge_gap_platform/controller/EmployeeController.java
+++ src/main/java/com/organizational/knowledge_gap_platform/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.organizational.knowledge_gap_platform.controller;
 
-import com.organizational.knowledge_gap_platform.entity.Employee;
+import com.organizational.knowledge_gap_platform.dto.EmployeeResponseDTO;
+import com.organizational.knowledge_gap_platform.dto.EmployeeUpdateDTO;
 import com.organizational.knowledge_gap_platform.service.EmployeeService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,8 +19,39 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
+    // Get All Employees
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
-    public ResponseEntity<List<Employee>> getAllEmployees() {
+    public ResponseEntity<List<EmployeeResponseDTO>> getAllEmployees() {
         return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
+    // Get Employee By id
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeResponseDTO> getEmployeeById(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(employeeService.getEmployeeById(id));
+    }
+
+    // Update Employee
+    @PutMapping("/{id}")
+    public ResponseEntity<EmployeeResponseDTO> updateEmployee(
+            @PathVariable Long id,
+            @RequestBody EmployeeUpdateDTO request) {
+
+        return ResponseEntity.ok(
+                employeeService.updateEmployee(id, request)
+        );
+    }
+
+    // Delete Employee
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEmployee(
+            @PathVariable Long id) {
+
+        employeeService.deleteEmployee(id);
+
+        return ResponseEntity.ok("Employee deleted successfully.");
     }
 }
--- src/main/java/com/organizational/knowledge_gap_platform/service/EmployeeService.java
+++ src/main/java/com/organizational/knowledge_gap_platform/service/EmployeeService.java
@@ -1,11 +1,17 @@
 package com.organizational.knowledge_gap_platform.service;
 
-import com.organizational.knowledge_gap_platform.entity.Employee;
+import com.organizational.knowledge_gap_platform.dto.EmployeeResponseDTO;
+import com.organizational.knowledge_gap_platform.dto.EmployeeUpdateDTO;
 
 import java.util.List;
 
 public interface EmployeeService {
 
-    List<Employee> getAllEmployees();
+    List<EmployeeResponseDTO> getAllEmployees();
 
+    EmployeeResponseDTO getEmployeeById(Long id);
+
+    EmployeeResponseDTO updateEmployee(Long id, EmployeeUpdateDTO request);
+
+    void deleteEmployee(Long id);
 }
