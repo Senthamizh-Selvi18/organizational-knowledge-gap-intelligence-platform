f8933f51a189ba813010696016c1ee384d4b6aa5
Swagata191 <majiswagata450@gmail.com>
2026-07-12 13:56:37 +0530

    Added notification system feature

---
+++ src/main/java/com/organizational/knowledge_gap_platform/exception/NotificationNotFoundException.java
@@ -0,0 +1,8 @@
+package com.organizational.knowledge_gap_platform.exception;
+
+public class NotificationNotFoundException extends RuntimeException {
+
+    public NotificationNotFoundException(String message) {
+        super(message);
+    }
+}
