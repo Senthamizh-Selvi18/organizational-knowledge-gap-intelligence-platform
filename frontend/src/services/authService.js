b800ea44d4d5cafb1b9be442a93a9b60fbca0cb7
TumpaDhara <tumpadhara857@gmail.com>
2026-08-15 13:59:11 +0530

    Fix register roles API endpoint

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -30,7 +30,7 @@ axios.interceptors.response.use(
 
 
 export const getRegisterableRoles = async () => {
-  const response = await axios.get(`${API_BASE_URL}/roles`);
+ const response = await axios.get(`${API_BASE_URL}/auth/roles`);
   return response.data;
 };
 
